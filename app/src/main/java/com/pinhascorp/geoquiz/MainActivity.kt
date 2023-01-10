479febd0da1e30ffd82cefc5a952deb58f7e086c
hakuna_as <hakunaforwork@gmail.com>
2023-01-10 15:55:54 +0200

    2 задание. реализовать кнопки без текста и нажатие назад

--- app/src/main/java/com/pinhascorp/geoquiz/MainActivity.kt
+++ app/src/main/java/com/pinhascorp/geoquiz/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
@@ -12,7 +13,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var previousButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(Questions(R.string.question_australia, true),
@@ -31,6 +33,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.nextButton)
+        previousButton = findViewById(R.id.previousButton)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener {view: View ->
@@ -42,6 +45,16 @@ class MainActivity : AppCompatActivity() {
         }
 
         nextButton.setOnClickListener { view: View ->
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        previousButton.setOnClickListener {
+            currentIndex = (currentIndex - 1).mod(questionBank.size)
+            updateQuestion()
+        }
+
+        questionTextView.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
         }
